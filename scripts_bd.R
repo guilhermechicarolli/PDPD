43b3a2e818084ea06a4a71ef169b9712447bfddc
Guilherme C. Chicarolli <68067314+guilhermechicarolli@users.noreply.github.com>
2020-12-04 19:21:22 -0300

    Update scripts_bd.R

--- scripts_bd.R
+++ scripts_bd.R
@@ -6,13 +6,59 @@
 library (tidyverse)
 library (ggplot2)
 
-#-------------------------------------------------------------------------------
-# BANCO ESPÉCIE LONCHOPHYLLA MORDAX
+#------------------------LONCHOPHYLLA MORDAX------------------------------------
 
-# Variável que representa o banco de dados da espécie L. mordax
-L_mordax <- read_csv('https://raw.githubusercontent.com/guilhermechicarolli/PDPD/main/Lonchophylla_mordax/data_lonchophylla_mordax.csv?token=AQHJ74WBF5E4MS5MNEYW6AK7ZE2GA')  
-L_mordax    
+#----- BANCO ESPÉCIE LONCHOPHYLLA MORDAX OBTIDOS NO SiBBr (61 registros)
 
-# meta dados das colunas
-colunas <- read_csv('https://raw.githubusercontent.com/guilhermechicarolli/PDPD/main/Lonchophylla_mordax/headings.csv?token=AQHJ74TEG6KQVHTUH2PJ3P27ZE2LA') 
-colunas    
+
+# Variável que representa o banco de dados da espécie L. mordax obtidos do SiBBr
+L_mordax1 <- read_csv('https://raw.githubusercontent.com/guilhermechicarolli/PDPD/main/Lonchophylla_mordax/data_lonchophylla_mordax.csv?token=AQHJ74WBF5E4MS5MNEYW6AK7ZE2GA')  
+L_mordax1 <- as_tibble(L_mordax1)    
+
+# Meta dados das colunas
+colunas1 <- read_csv('https://raw.githubusercontent.com/guilhermechicarolli/PDPD/main/Lonchophylla_mordax/headings.csv?token=AQHJ74TEG6KQVHTUH2PJ3P27ZE2LA') 
+colunas_sibbr <- as_tibble(colunas1)   
+
+#---------
+
+# Remoção de colunas redundantes ou irrelevantes ao projeto
+L_mordax1 <- L_mordax1 %>%
+    select(-'Institution ID', -'Data Resource ID', -'Collection ID', -'Institution Code',
+           -'Collection Code', -'identified _ by', -'identified _ date', -'Collector', 
+           -'Minimum elevation in meters', -'Maximum elevation in meters', -'Scientific Name - original',
+           -'Vernacular name - original', -'Sex', -'Vernacular name', -'Catalogue Number')
+
+# Remoção de colunas preenchidas apenas por NAs ou valores fixos
+L_mordax1 <- L_mordax1 %>%
+    select(-'Minimum depth in meters', -'Maximum depth in meters', -'Coordinate Uncertainty in Metres',
+           -'Country - parsed', -'State - parsed', -'Occurrence status', 
+           -('raw _ sampling _ protocol' : 'Type status not recognised'), -'Basis Of Record - original',
+           -'Taxon Rank')
+
+# Remoção de observações sem referência geográfica de latitude e longitude
+L_mordax1 <- L_mordax1 %>%
+    filter(!is.na(`Latitude - original` | `Longitude - original`))
+
+# Remoção de observações com a mesma referência geográfica de latitude e longitude, permanecendo apenas uma
+L_mordax1 <- L_mordax1 %>%
+    distinct(`Latitude - original`, `Longitude - original`, .keep_all = TRUE)
+
+view(L_mordax1)
+view(colunas_sibbr)
+
+
+
+#-------- BANCO DE DADOS DA ESPÉCIE LONCHOPHYLLA MORDAX OBTIDOS NO GBIF (12 REGISTROS)
+
+L_mordax2 <- read_delim('https://raw.githubusercontent.com/guilhermechicarolli/PDPD/main/Lonchophylla_mordax/GBIF/occurrence.txt', delim='\t')
+L_mordax2 <- as_tibble(L_mordax2)
+
+# Removendo colunas com apenas NAs
+L_mordax2 <- L_mordax2 %>%
+    select_if(~all(!is.na(.)))
+
+view(L_mordax2)
+
+# Remoção de observações com a mesma referência geográfica de latitude e longitude, permanecendo apenas uma
+L_mordax2 <- L_mordax2 %>%
+    distinct(decimalLatitude, decimalLongitude, .keep_all = TRUE)
