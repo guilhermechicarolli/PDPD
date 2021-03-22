6cd47b139ea75c8389a86c619a91d2198f4d6c49
guilhermechicarolli <gui.chicarolli@outlook.com>
2021-03-22 13:16:19 -0300

    plot L bokermanni

---
+++ Scripts/Plot_L_bokermanni_registros.R
@@ -0,0 +1,100 @@
+################################################################################
+#
+# FIGURE 1
+# Geographic distribution of sampling sites by study type
+#
+################################################################################
+
+#Delete all previous objects
+rm(list= ls())
+
+
+################################################################################
+##### LOAD THE PACKAGES
+################################################################################
+
+if (!require(dplyr)) install.packages('dplyr')
+if (!require(ggplot2)) install.packages('ggplot2')
+if (!require(grDevices)) install.packages('grDevices')
+if (!require(sf)) install.packages('sf')
+if (!require(rnaturalearth)) install.packages('rnaturalearth')
+if (!require(rnaturalearthdata)) install.packages('rnaturalearthdata')
+if (!require(ggspatial)) install.packages('ggspatial')
+if (!require(rgeos)) install.packages('rgeos')
+
+
+################################################################################
+##### IMPORT THE DATA
+################################################################################
+
+
+# Import the data set with site coordinates
+sites <- read.csv("Dados/registros_L_bokermanni.csv", encoding = "UTF-8")
+
+# Check the data
+class(sites)
+str(sites)
+head(sites)
+
+# Select the columns with the coordinates and study types
+sites_short <- sites %>% 
+    dplyr::select(Latitude, Longitude, Estado)
+
+#Check the data
+head(sites_short)
+
+# Load the world map from the mapdata package
+world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
+
+
+################################################################################
+##### PLOT THE MAP
+################################################################################
+
+
+# Make the base map
+g1 <- ggplot(data = world) +
+    geom_sf(colour = "white", fill = "#d3d3d3") +
+    coord_sf(xlim = c(-50, -30), ylim = c(-30,0), expand = FALSE) +
+    theme_bw() + 
+    # Plot the sites
+    geom_point(data = sites_short, aes(x = Longitude, y = Latitude, 
+                                       colour = Estado), 
+               alpha = 0.6, size = 2) +
+    # Customize the colors and labels
+    scale_color_manual(values = c("#C59F00","#d3d3d3","#C59F00","#C59F00", "#C59F00")) + 
+    labs(colour = "Estado", x = "Longitude", y = "Latitude") +
+    theme(panel.grid = element_blank(),
+          legend.text = element_text(size = 11),
+          legend.title = element_text(face = "bold", size = 11),
+          axis.text = element_text(size = 11, colour = "black"),
+          axis.title.x = element_text(size = 11, colour = "black", vjust = -4,
+                                      face = "bold"),
+          axis.title.y = element_text(size = 11, colour = "black", vjust = 1,
+                                      face = "bold"),
+          legend.position = c(0.8,0.3),
+          legend.background = element_rect(fill = "NA"),
+          legend.key = element_rect(fill = "NA"),
+          plot.margin = unit(rep(0.5,4), "lines")) +
+    # Add a scale bar
+    ggspatial::annotation_scale(location = "bl", width_hint = 0.2,
+                                bar_cols = c("grey30", "white")) +
+    # Add a north arrow
+    ggspatial::annotation_north_arrow(location = "tr", which_north = "true",
+                                      height = unit(1.5, "cm"), 
+                                      width = unit(1.5, "cm"),
+                                      style = ggspatial::north_arrow_fancy_orienteering(
+                                          fill = c("white","grey30")))
+
+# See the map
+g1
+
+# Export the map as a PNG image
+png("./Dados/Figure_1.png", res = 300,
+    width = 2000, height = 2200, unit = "px")
+g1
+
+dev.off()
+
+
+################################ END ###########################################
