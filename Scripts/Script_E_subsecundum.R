2bd9bb0ddc0bf3b80856215cefd6bb33adea1057
guilhermechicarolli <gui.chicarolli@gmail.com>
2021-04-01 21:56:10 -0300

    Adicionei os registros do GBIF da E subsecundum e limpei parte dos dados no script E subsecundum

--- Scripts/Script_E_subsecundum.R
+++ Scripts/Script_E_subsecundum.R
@@ -1,14 +1,14 @@
-#-------- SCRIPTS DO BANCO DE DADOS DA ESPÉCIE ENCHOLIRIUM SUBSECUNDUM --------------#
+#--------- SCRIPTS DO BANCO DE DADOS DA ESPÉCIE ENCHOLIRIUM SUBSECUNDUM --------------#
 
 
 # Carregando bibliotecas necessárias
 library(tidyverse)
 
 
-#-------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------
 
-# Importando o arquivo CSV com os registros:
-registros <- read.csv("Dados/speciesLink_E_subsecundum.csv", 
+# Importando o arquivo CSV com os registros do speciesLink:
+registros <- read.csv("Dados/DataSets_temporarios/speciesLink_E_subsecundum.csv", 
                       encoding = "UTF-8") %>%
     as_tibble(registros)
 
@@ -27,4 +27,27 @@ path <- "./Dados/"
 write_csv(registros, paste(path,'registros_E_subsecundum.csv'))
 
 
+#--------------------------------------------------------------------------------------
 
+
+# Importando o arquivo CSV com os registros do GBIF:
+registros2 <- read.csv("Dados/DataSets_temporarios/GBIF_E_subsecundum.txt", sep='\t', 
+                      encoding = "UTF-8") %>%
+    as_tibble(registros)
+
+# Verificando os dados
+head(registros2)
+view(registros2)
+
+# Selecionando as colunas de interesse para o trabalho
+registros2 <- registros2 %>%
+    select(catalogNumber, stateProvince, county, municipality, decimalLongitude, decimalLatitude, 
+           year, recordedBy, datasetName) %>%
+    drop_na(decimalLatitude, decimalLongitude)
+
+# Preenchendo células vazias por NA
+registros2[registros2 == ""] <- NA
+
+# Salvando o data_frame resultante na pasta Dados
+path <- "./Dados/DataSets_temporarios"
+write_csv(registros2, paste(path,'registros2_E_subsecundum.csv'))
