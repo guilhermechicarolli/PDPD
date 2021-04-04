7657a691c947af7c9e2ed92fa5a3368c3ccd9527
guilhermechicarolli <gui.chicarolli@outlook.com>
2021-04-03 22:38:08 -0300

    Adicionei os registros da E subsecundum do banco de dados do SiBBr, os registros totais estão no arquivo registros_E_subsecundum

--- Scripts/Script_E_subsecundum.R
+++ Scripts/Script_E_subsecundum.R
@@ -33,7 +33,7 @@ write_csv(registros, paste(path,'registros_E_subsecundum.csv'))
 # Importando o arquivo CSV com os registros do GBIF:
 registros2 <- read.csv("Dados/DataSets_temporarios/GBIF_E_subsecundum.txt", sep='\t', 
                       encoding = "UTF-8") %>%
-    as_tibble(registros)
+    as_tibble(registros2)
 
 # Verificando os dados
 head(registros2)
@@ -41,13 +41,40 @@ view(registros2)
 
 # Selecionando as colunas de interesse para o trabalho
 registros2 <- registros2 %>%
-    select(catalogNumber, stateProvince, county, municipality, decimalLongitude, decimalLatitude, 
-           year, recordedBy, datasetName) %>%
+    select(catalogNumber, stateProvince, county, municipality, decimalLongitude, 
+           decimalLatitude, year, recordedBy, datasetName) %>%
     drop_na(decimalLatitude, decimalLongitude)
 
 # Preenchendo células vazias por NA
 registros2[registros2 == ""] <- NA
 
 # Salvando o data_frame resultante na pasta Dados
-path <- "./Dados/DataSets_temporarios"
+path <- "./Dados/DataSets_temporarios/"
 write_csv(registros2, paste(path,'registros2_E_subsecundum.csv'))
+
+
+#--------------------------------------------------------------------------------------
+
+
+# Importando o arquivo CSV com os registros do SiBBr:
+registros3 <- read.csv("Dados/DataSets_temporarios/SiBBr_E_subsecundum.csv", 
+                       encoding = "UTF-8") %>%
+    as_tibble(registros3)
+
+# Verificando os dados
+head(registros3)
+view(registros3)
+
+# Selecionando as colunas de interesse para o trabalho e removendo colunas sem coordenadas
+registros3 <- registros3 %>%
+    select(Catalogue.Number, State...parsed,Geodetic.datum...original,Locality,
+           Longitude...original,Latitude...original,Year,Collector,Data.Resource.Name) %>%
+    drop_na(Longitude...original,Latitude...original) %>%
+    filter(!(Longitude...original == 0 | Latitude...original == 0))
+
+# Preenchendo células vazias por NA
+registros3[registros3 == ""] <- NA
+
+# Salvando o data_frame resultante na pasta Dados
+path <- "./Dados/DataSets_temporarios/"
+write_csv(registros3, paste(path,'registros3_E_subsecundum.csv'))
