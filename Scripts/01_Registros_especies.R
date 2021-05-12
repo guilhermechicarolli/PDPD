e5b224f99679cd05ce8ec348c514dafa4ceb7bd9
guilhermechicarolli <gui.chicarolli@outlook.com>
2021-05-12 09:57:10 -0300

    adicionei o maxent e organizei os scripts em ordem numérica

--- Scripts/01_Registros_especies.R
+++ Scripts/01_Registros_especies.R
@@ -1,43 +1,53 @@
-#--------- SCRIPTS DO BANCO DE DADOS DA ESPÉCIE ENCHOLIRIUM SUBSECUNDUM --------------#
+############## SCRIPTS DO PROJETO DE PDPD ##############
+
+# 1. SCRIPTS DO BANCO DE DADOS DA ESPÉCIE ENCHOLIRIUM SUBSECUNDUM
+# 2. SCRIPTS DO BANCO DE DADOS DA ESPÉCIE LONCHOPHYLLA BOKERMANNI
+
+################################################################################
+
+#--------- 1. SCRIPTS DO BANCO DE DADOS DA ESPÉCIE 
+#                ENCHOLIRIUM SUBSECUNDUM --------------#
 
 
-# Carregando bibliotecas necessárias
-library(tidyverse)
+##### CARREGAMENTO DOS PACOTES NECESSÁRIOS #####
+
+if (!require(tidyverse)) install.packages('tidyverse')
 
 
-#--------------------------------------------------------------------------------------
+##### IMPORTANDO O ARQUIVO CSV COM OS REGISTROS DO SPECIESLINK #####
 
-# Importando o arquivo CSV com os registros do speciesLink:
-registros <- read.csv("Dados/DataSets_temporarios/speciesLink_E_subsecundum.csv", 
+registros <-read.csv("Dados/DataSets_temporarios/speciesLink_E_subsecundum.csv", 
                       encoding = "UTF-8") %>%
     as_tibble(registros)
+
 
 # Verificando os dados
 head(registros)
 view(registros)
 
+
 # Selecionando as colunas de interesse para o trabalho
 registros <- registros %>%
-    select(catalognumber, stateprovince, county, locality, longitude_mun, latitude_mun, 
-           yearcollected, collector, institutioncode)
+    select(catalognumber, stateprovince, county, locality, longitude_mun, 
+           latitude_mun, yearcollected, collector, institutioncode)
+
 
 # Salvando o data_frame resultante na pasta Dados
-
 path <- "./Dados/"
 write_csv(registros, paste(path,'registros_E_subsecundum.csv'))
 
 
-#--------------------------------------------------------------------------------------
+##### IMPORTANDO O ARQUIVO CSV COM OS REGISTROS DO GBIF #####
 
-
-# Importando o arquivo CSV com os registros do GBIF:
-registros2 <- read.csv("Dados/DataSets_temporarios/GBIF_E_subsecundum.txt", sep='\t', 
-                      encoding = "UTF-8") %>%
+registros2 <- read.csv("Dados/DataSets_temporarios/GBIF_E_subsecundum.txt",
+                       sep='\t', encoding = "UTF-8") %>%
     as_tibble(registros2)
+
 
 # Verificando os dados
 head(registros2)
 view(registros2)
+
 
 # Selecionando as colunas de interesse para o trabalho
 registros2 <- registros2 %>%
@@ -45,25 +55,27 @@ registros2 <- registros2 %>%
            decimalLatitude, year, recordedBy, datasetName) %>%
     drop_na(decimalLatitude, decimalLongitude)
 
+
 # Preenchendo células vazias por NA
 registros2[registros2 == ""] <- NA
+
 
 # Salvando o data_frame resultante na pasta Dados
 path <- "./Dados/DataSets_temporarios/"
 write_csv(registros2, paste(path,'registros2_E_subsecundum.csv'))
 
 
-#--------------------------------------------------------------------------------------
+##### IMPORTANDO O ARQUIVO CSV COM OS REGISTROS DO SIBBR #####
 
-
-# Importando o arquivo CSV com os registros do SiBBr:
 registros3 <- read.csv("Dados/DataSets_temporarios/SiBBr_E_subsecundum.csv", 
                        encoding = "UTF-8") %>%
     as_tibble(registros3)
 
+
 # Verificando os dados
 head(registros3)
 view(registros3)
+
 
 # Selecionando as colunas de interesse para o trabalho e removendo colunas sem coordenadas
 registros3 <- registros3 %>%
@@ -72,9 +84,29 @@ registros3 <- registros3 %>%
     drop_na(Longitude...original,Latitude...original) %>%
     filter(!(Longitude...original == 0 | Latitude...original == 0))
 
+
 # Preenchendo células vazias por NA
 registros3[registros3 == ""] <- NA
+
 
 # Salvando o data_frame resultante na pasta Dados
 path <- "./Dados/DataSets_temporarios/"
 write_csv(registros3, paste(path,'registros3_E_subsecundum.csv'))
+
+
+################################################################################
+
+#-------- SCRIPTS DO BANCO DE DADOS DA ESPÉCIE 
+#               LONCHOPHYLLA BOKERMANNI --------------#
+
+
+# Exportando o arquivo CSV criado:
+registros <- read.csv("Dados/registros_L_bokermanni.csv", 
+                      encoding = "UTF-8") %>%
+    as_tibble(registros)
+
+
+# Verificando os dados
+head(registros)
+view(registros)
+
