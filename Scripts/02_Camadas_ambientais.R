b2589964f6249bde5e5adc03ed62c5bc6f63753e
guilhermechicarolli <gui.chicarolli@gmail.com>
2021-05-19 15:35:44 -0300

    tentativa de carregar os shapefiles do Brasil

--- Scripts/02_Camadas_ambientais.R
+++ Scripts/02_Camadas_ambientais.R
@@ -1,7 +1,6 @@
 ############## SCRIPTS DO PROJETO DE PDPD ##############
 
-# 1. CONSTRUÇÃO DA MÁSCARA COM BASE NO SHAPE FILE DOS 
-#    BIOMAS DA MATA ATLÂNTICA, CAATINGA E CERRADO
+# 1. CONSTRUÇÃO DA MÁSCARA COM BASE NO SHAPE FILE DO BRASIL
 #
 # 2. TRATAMENTO DAS VARIÁVEIS AMBIENTAIS DO PRESENTE: Cortes usando
 #    a máscara criada anteriormente, reprojeção e conversão 
@@ -18,6 +17,8 @@
 if (!require(raster)) install.packages('raster')
 if (!require(rgdal)) install.packages('rgdal')
 if (!require(sp)) install.packages('sp')
+if (!require(sf)) install.packages('sf')
+
 
 # Permite que dados espaciais sejam associados com o sistema de coordenadas,
 # criando uma projeção que pode ser utilizada nos rasters
@@ -29,34 +30,28 @@ proj_WGS <- sp::CRS(
 ################################################################################
 
 #--------- 1. CONSTRUÇÃO DA MÁSCARA COM BASE 
-#              NO SHAPE FILE DOS BIOMAS ---------#
+#               NO SHAPE FILE DO BRASIL ---------#
 
 
-# Carregamento dos shape files dos biomas
-biomas <- rgdal::readOGR("Dados/Biomas_250mil/lm_bioma_250.shp")
+# Carregamento dos shape files do brasil
+brasil <- rgdal::readOGR(dsn="Dados/Mascaras/", layer='UFEBRASIL')
+brasil <- shapefile("Dados/Mascaras/UFEBRASIL.shp")
+brasil <- read_sf("Dados/Mascaras/UFEBRASIL.shp")
 
-head(biomas)
+
+head(brasil)
 
 # Verificação dos dados a partir do plot do mapa
-plot(biomas)
-
-# Selecionar apenas os biomas do Cerrado, Caatinga e Mata Atlântica
-CE_CA_MA <-biomas[biomas$Bioma=="Cerrado" | biomas$Bioma=="Caatinga" |
-                      biomas$Bioma=="Mata AtlÃ¢ntica",]
-
-head(CE_CA_MA)
-
-# Verificação dos dados
-plot(CE_CA_MA)
+plot(brasil)
 
 # Conversão para SpatialPolygonsDataFrame
-CE_CA_MA <- as(CE_CA_MA, "SpatialPolygonsDataFrame")
+brasil <- as(brasil, "SpatialPolygonsDataFrame")
 
 # Acréscimo da projeção 
-raster::crs(CE_CA_MA) <- proj_WGS
+raster::crs(brasil) <- proj_WGS
 
 # Salvar a máscara criada na pasta Dados/Mascaras
-rgal::writeOGR(CE_CA_MA, "./Dados/Mascaras", "mascara_biomas",
+rgal::writeOGR(brasil, "./Dados/Mascaras", "mascara_brasil",
                driver="ESRI Shapefile", overwrite_layer = T)
 
 
@@ -68,7 +63,7 @@ rgal::writeOGR(CE_CA_MA, "./Dados/Mascaras", "mascara_biomas",
 
 
 # Carregamento da máscara criada no passo 01
-mascara <- shapefile('Dados/Mascaras/mascara_biomas.shp')
+mascara <- shapefile('Dados/Mascaras/mascara_brasil.shp')
 
 # Adicionar a projeção definida anteriormente
 raster::crs(mascara) <- proj_WGS
