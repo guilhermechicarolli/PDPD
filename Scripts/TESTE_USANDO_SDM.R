deb4d7c7e633e7b06f5ce8a244bf74b5ba5ffb91
guilhermechicarolli <gui.chicarolli@outlook.com>
2021-08-25 18:45:42 -0300

    teste modelagem usando sdm

--- Scripts/TESTE_USANDO_SDM.R
+++ Scripts/TESTE_USANDO_SDM.R
@@ -78,6 +78,72 @@ plot(m@models$species$maxent$`1`@object)
 gui(m)
 
 p1 <- predict(m, bioc, filename='./Resultados_teste_SDM/presente.img')
+p1
 
-en1 <- ensemble(m, bio, filename = './Resultados_teste_SDM/ensemble.img', 
-                setting =list(id=c(1,3,4,8,12), method='weighted', stat='tss', opt=2))
+
+en1 <- ensemble(m, bioc, filename = './Resultados_teste_SDM/ensemble.img', 
+                setting =list(method='weighted', stat='tss', opt=2))
+
+raster::crs(en1) <- proj_WGS
+en1
+
+plot(p1)
+
+plot(en1)
+
+#### FUTURO
+
+biof <- list.files(path='./Dados/Camadas_biovars_res_2.5_brasil/selec_subs_45/',
+                   pattern = '.asc', full.names = TRUE)
+biof <- raster::stack(biof)
+
+
+en2 <- ensemble(m, biof, filename='./Resultados_teste_SDM/futuro.img',
+                setting =list(method='weighted', stat='tss', opt=2))
+
+plot(en2)
+
+#-------
+cl<-colorRampPalette(c('#3E49BB', '#3498DB', 'yellow', 'orange', 'red', 'darkred'))
+
+plot(en1, col=cl(200))
+plot(en2, col=cl(200))
+
+mapview(stack(en1,en2)) + spg
+
+proj4string(spg) <- projection(en1)
+
+
+#-------
+
+ch <- en2 - en1
+cl2<-colorRampPalette(c('darkred', 'red', 'orange', 'yellow', '#3498DB', '#3E49BB'))
+
+plot(ch, col=cl2(200))
+
+
+#-------
+
+df <- as.data.frame(d)
+df <- data.frame(species=df$species, coordinates(d))
+xy = as.matrix(df[,c('x', 'y')])
+head(xy)
+
+p<-raster::extract(en1,xy)
+
+ev <- evaluates(df$species,p)
+ev@statistics
+
+ev@threshold_based
+
+th <- ev@threshold_based$threshold[2]
+th
+
+pa1 <- raster(en1)
+pa1[] <- ifelse(en1[] >= th, 1,0)
+plot(pa1)
+
+
+pa2 <- raster(en2)
+pa2[] <- ifelse(en2[] >= 0.015, 1,0)
+plot(pa2)
